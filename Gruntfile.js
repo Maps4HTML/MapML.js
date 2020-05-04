978c3a255a803fe8e7f2c35627966395849a0465
Peter Rushforth <peter.rushforth@gmail.com>
2020-05-04 17:19:14 -0400

    First (perhaps) workable pass at setting up a testing framework, using: karma, mocha, and chai testing framework components. The intent is to extend tests based on the layerTests.js file.  Other test js files in the test directory are so far ideas about testing the custom elements using Selenium, or perhaps puppetteer, however the former doesn't yet support access to the shadowDOM of custom elements, so slightly useless for that purpose.  The latter I haven't tested much, but it's chrome-specific so also slightly off course.

--- Gruntfile.js
+++ Gruntfile.js
@@ -135,6 +135,11 @@ module.exports = function(grunt) {
     },
     clean: {
       dist: ['dist']
+    },
+    karma: {
+      unit: {
+        configFile: 'test/karma.conf.js'
+      }
     }
   });
 
@@ -145,7 +150,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
- /* grunt.loadNpmTasks('grunt-processhtml'); */
+  grunt.loadNpmTasks('grunt-karma');
+/* grunt.loadNpmTasks('grunt-processhtml'); */
 
 
   grunt.registerTask('test', ['jshint']);
