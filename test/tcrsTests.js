692cd60e9c2edb258017d2d59b309555c2a406f8
Peter Rushforth <peter.rushforth@canada.ca>
2020-05-11 20:31:45 -0400

    Merge origin/master
    Conflicts:
    	package.json
    	test/karma.conf.js
    	test/layerTests.js

--- test/tcrsTests.js
+++ test/tcrsTests.js
@@ -11,4 +11,15 @@ describe('TCRS Tests', function() {
     });
     
   });
+  // for (OSMTILE, CBMTILE, WGS84, APSTILE) 
+    // options.origin at expected location
+    // options.bounds equal to expected bounds
+    // options.resolutions has exactly the same number of zoom levels as expected
+    // options.resolutions[zoom] === to expexted value
+    // if it exists, .scales[] has the same dimension as options.resolutions
+    // .scales[zoom] is equal to the inverse of options.resolutions[zoom]
+    // for (crs in tcrs,pcrs,gcrs,map,tile,tilematrix)
+    //   options.crs.(tcrs,pcrs,gcrs,map,tile,tilematrix) are deep equal to expected
+    
+  
 });
