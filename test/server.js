b9f0472c284dda32d88b43db5958cbcef87d059e
Ahmad Ayubi <aayub041@uottawa.ca>
2020-06-01 17:41:25 -0400

    integrated peters cached tile data and test files

--- test/server.js
+++ test/server.js
@@ -7,7 +7,12 @@ const port = 30001;
 app.use(express.static(path.join(__dirname, "../src")));
 //then loads in the index file
 app.use(express.static(path.join(__dirname, "e2e")));
-app.use("/data", express.static(path.join(__dirname, "e2e/data")));
+app.use("/data", express.static(path.join(__dirname, "e2e/data/tiles/cbmt")));
+app.use(
+  "/data/2",
+  express.static(path.join(__dirname, "e2e/data/tiles/cbmt/2"))
+);
+
 //lastly loads the dist, but the original src files have priority
 app.use(express.static(path.join(__dirname, "../dist")));
 console.log("Running on localhost:" + port);
