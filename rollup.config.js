5fcfc3f1ede7b4272b163527af6bb156be90f6b5
Ahmad Ayubi <55214462+ahmadayubi@users.noreply.github.com>
2020-08-12 16:30:51 -0400

    Revert "Restructures src directory and implements RollUpJS (#128)"
    This reverts commit 749173c0b97cab1f2a654d45dee1758780ef9e3d.

--- rollup.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'bundleTest/mapml.js',
-    format: 'iife',
-  },
-};
--- src/mapml/utils/Constants.js
+++
@@ -1,3 +0,0 @@
-export const TILE_SIZE = 256;
-export const FALLBACK_PROJECTION = "OSMTILE";
-export const FALLBACK_CS = "TILEMATRIX";
