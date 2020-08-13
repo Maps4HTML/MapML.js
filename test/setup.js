cccbba4cffeeb89ea539343120fc22a6acf51f8e
Ahmad Ayubi <aayub041@uottawa.ca>
2020-08-13 14:37:26 -0400

    Adjusts jest test setup to work with RollUpJS

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,4 @@
 require("../dist/leaflet-src.js");
 require("../dist/proj4-src.js");
 require("../dist/proj4leaflet.js");
-require("../src/mapml.js");
-
-/* 'dist/leaflet-src.js',
-      'dist/proj4-src.js',
-      'dist/proj4leaflet.js',
-      'src/mapml.js', */
+require("../dist/mapml.js");
