9776d411b1ce79d070fd098c5f8ad33be04db7dc
Ahmad Ayubi <aayub041@uottawa.ca>
2020-12-15 15:41:15 -0500

    Adds tests

--- src/nav.js
+++ src/nav.js
@@ -7,6 +7,7 @@ import './proj4leaflet.js';
     function (t) {
       if (!t.code || !t.proj4string || !t.projection || !t.definition) return {};
       M[t.projection] = new L.Proj.CRS(t.code, t.proj4string, t.definition);
+      M[t.projection.toUpperCase()] = M[t.projection];
       return { crs: M[t.projection], projection: t.projection, };
     };
 })();
