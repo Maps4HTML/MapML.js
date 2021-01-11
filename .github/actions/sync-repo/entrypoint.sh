dfd11a5efbd8307ff257057a0270893cc8dc0708
Ahmad Ayubi <aayub041@uottawa.ca>
2021-01-11 11:35:42 -0500

    Update for MapML Repo

--- .github/actions/sync-repo/entrypoint.sh
+++ .github/actions/sync-repo/entrypoint.sh
@@ -26,7 +26,7 @@ git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
-cp -r $INPUT_SOURCE_FOLDER/. $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+cp -rf $INPUT_SOURCE_FOLDER/. $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
 cd "$CLONE_DIR"
 
 echo "Adding git commit"
