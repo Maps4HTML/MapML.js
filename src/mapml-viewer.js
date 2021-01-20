c47db9d7a4016607373935132ad2c5e1ea22900b
Malvoz <26493779+Malvoz@users.noreply.github.com>
2021-01-20 23:16:44 +0100

    Fix to allow .leaflet-grab for a "grabbing" cursor
    The `.leaflet-dragging .leaflet-grab` selector to set a "grabbing" cursor doesn't work because `leaflet-dragging` is set on the document body (is outside the shadow boundary). This is a workaround to allow usage of `leaflet-grab`.

--- src/mapml-viewer.js
+++ src/mapml-viewer.js
@@ -129,6 +129,12 @@ export class MapViewer extends HTMLElement {
     `}` +
     `:host(.leaflet-drag-target) .leaflet-control {` +
     `pointer-events: none;` + // Prevent `:hover` styles from applying to controls when the user is panning the map display and the cursor happens to move over a control.
+    `}` +
+    `:host(.leaflet-drag-target) .leaflet-grab {` +
+    `cursor: move;` +
+  	`cursor: -webkit-grabbing;` +
+  	`cursor:    -moz-grabbing;` +
+  	`cursor:         grabbing;` +
     `}`;
     
     // Hide all (light DOM) children of the map element.
