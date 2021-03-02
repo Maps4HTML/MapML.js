f0cb14bcbfb56811bf151ba1d0de2930551329dc
Ahmad Ayubi <aayub041@uottawa.ca>
2021-03-02 16:29:12 -0500

    Adds custom marker

--- src/mapml/layers/FeatureLayer.js
+++ src/mapml/layers/FeatureLayer.js
@@ -342,16 +342,6 @@ export var MapMLFeatures = L.FeatureGroup.extend({
         latlng, latlngs, coordinates, member, members, linestrings;
 
     coordsToLatLng = coordsToLatLng || this.coordsToLatLng;
-    var pointOptions = {  opacity: vectorOptions.opacity ? vectorOptions.opacity : null,
-                          icon: L.icon(
-                            { iconUrl: vectorOptions.imagePath+"marker-icon.png",
-                              iconRetinaUrl: vectorOptions.imagePath+"marker-icon-2x.png",
-                              shadowUrl: vectorOptions.imagePath+"marker-shadow.png",
-                              iconSize: [25, 41],
-                              iconAnchor: [12, 41],
-                              popupAnchor: [1, -34],
-                              shadowSize: [41, 41]
-                            })};
     
     var cs = geometry.getAttribute("cs") || nativeCS;
 
--- src/mapml/layers/SVGMarker.js
+++ src/mapml/layers/SVGMarker.js
@@ -1,45 +1,27 @@
 export var SVGMarker = L.Path.extend({
   options: {
-    stroke: false,
-    color: '00AEEF',
+    stroke: true,
+    fillColor: '#2e91bf',
+    color: '#ffffff',
     fill: true,
     className: "mapml-marker",
+    fillOpacity: 1,
+    weight: 1,
   },
 
   initialize: function (latlng, options) {
     L.setOptions(this, options);
     this._latlng = L.latLng(latlng);
-    this._svg = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                    viewBox="0 0 365 560" enable-background="new 0 0 365 560" xml:space="preserve">
-                    <g>
-                      <path fill="#00AEEF" d="M182.9,551.7c0,0.1,0.2,0.3,0.2,0.3S358.3,283,358.3,194.6c0-130.1-88.8-186.7-175.4-186.9
-                        C96.3,7.9,7.5,64.5,7.5,194.6c0,88.4,175.3,357.4,175.3,357.4S182.9,551.7,182.9,551.7z M122.2,187.2c0-33.6,27.2-60.8,60.8-60.8
-                        c33.6,0,60.8,27.2,60.8,60.8S216.5,248,182.9,248C149.4,248,122.2,220.8,122.2,187.2z"/>
-                    </g>
-                    </svg>`;
-    this._icon = L.icon({
-      iconUrl: "data:image/svg+xml;base64," + btoa(this._svg),
-    });
-
   },
 
   _project: function () {
     this._rings = [];
     this._point = this._map.latLngToLayerPoint(this._latlng);
-    //substract the pixel origin from the pixel coordinates to get the location relative to map viewport
-    this._rings.push(L.point(this._point.x, this._point.y));
-    this._rings.push(L.point(this._point.x - 10, this._point.y - 30));
-    this._rings.push(L.point(this._point.x, this._point.y - 40));
-    this._rings.push(L.point(this._point.x + 10, this._point.y - 30));
-
-    //leaflet SVG renderer looks for and array of arrays to build polygons,
-    //in this case it only deals with a rectangle so one closed array or points
-    this._parts = [this._rings];
   },
 
   _update: function () {
     if (!this._map) return;
-    this._renderer._updatePoly(this, true); //passing true creates a closed path i.e. a rectangle
+    this._path.setAttribute("d", this.generateMarkerPath(this._point));
   },
 
   getLatLngs: function () {
@@ -48,6 +30,10 @@ export var SVGMarker = L.Path.extend({
 
   getCenter: function () {
     return this._latlng;
+  },
+
+  generateMarkerPath: function (p) {
+    return `M${p.x} ${p.y} L${p.x - 12.5} ${p.y - 30} C${p.x - 12.5} ${p.y - 50}, ${p.x + 12.5} ${p.y - 50}, ${p.x + 12.5} ${p.y - 30} L${p.x} ${p.y}z`;
   },
 
 });
