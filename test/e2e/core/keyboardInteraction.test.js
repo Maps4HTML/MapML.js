9521610d4ecac740447fa4eab4c3e81f275c0862
Malvoz <26493779+Malvoz@users.noreply.github.com>
2021-03-18 05:40:18 +0100

    Fix test expectation

--- test/e2e/core/keyboardInteraction.test.js
+++ test/e2e/core/keyboardInteraction.test.js
@@ -225,7 +225,7 @@ jest.setTimeout(50000);
             let tooltipCount = await page.$eval("div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-tooltip-pane", div => div.childElementCount);
 
             expect(tooltipCount).toEqual(1);
-            expect(f).toEqual("M330 83L480 83L480 339L330 339z");
+            expect(f).toEqual("M330 83L483 83L483 339L330 339z");
           });
 
           test("[" + browserType + "]" + " Next feature button focuses next feature", async () => {
