8879e481479f849b75378cee0ae9d18a311ded02
Ahmad Ayubi <aayub041@uottawa.ca>
2021-04-23 12:05:46 -0400

    Adds link preview

--- src/mapml/features/feature.js
+++ src/mapml/features/feature.js
@@ -56,7 +56,9 @@ export var Feature = L.Path.extend({
    * @param leafletLayer
    */
   attachLinkHandler: function (path, link, leafletLayer) {
-    let dragStart; //prevents click from happening on drags
+    let dragStart, container = document.createElement('div'), p = document.createElement('p'), hovered = false;
+    container.classList.add('mapml-link-preview');
+    container.appendChild(p);
     L.DomEvent.on(path, 'mousedown', e => dragStart = {x:e.clientX, y:e.clientY}, this);
     L.DomEvent.on(path, "mouseup", (e) => {
       let onTop = true, nextLayer = this.options._leafletLayer._layerEl.nextElementSibling;
@@ -75,6 +77,27 @@ export var Feature = L.Path.extend({
       L.DomEvent.stop(e);
       if(e.keyCode === 13 || e.keyCode === 32)
         M.handleLink(link, leafletLayer);
+    }, this);
+    L.DomEvent.on(path, 'mouseenter', (e) => {
+      L.DomEvent.stop(e);
+      hovered = true;
+      let resolver = document.createElement('a'), mapWidth = this._map.getContainer().clientWidth;
+      resolver.href = link.url;
+      p.innerHTML = resolver.href;
+
+      this._map.getContainer().appendChild(container);
+
+      while(p.clientWidth > mapWidth/2){
+        p.innerHTML = p.innerHTML.substring(0, p.innerHTML.length - 5) + "...";
+      }
+      setTimeout(()=>{
+        if(hovered) p.innerHTML = resolver.href;
+      }, 1000);
+    }, this);
+    L.DomEvent.on(path, 'mouseout', (e) => {
+      L.DomEvent.stop(e);
+      hovered = false;
+      this._map.getContainer().removeChild(container);
     }, this);
   },
 
