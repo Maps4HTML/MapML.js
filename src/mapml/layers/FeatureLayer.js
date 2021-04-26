a5bcc0fa0bc9a8c14241b818437f35fd1f361e89
Ahmad Ayubi <55214462+ahmadayubi@users.noreply.github.com>
2021-04-26 17:22:17 -0400

    Point fix (#426)
    * Point fix
    
    * Add test for no geometry
    
    * Fix options overriding
    
    * Fix order of options set

--- src/mapml/layers/FeatureLayer.js
+++ src/mapml/layers/FeatureLayer.js
@@ -316,9 +316,9 @@ export var MapMLFeatures = L.FeatureGroup.extend({
     },
   geometryToLayer: function (mapml, vectorOptions, nativeCS, zoom, title) {
     let geometry = mapml.tagName.toUpperCase() === 'FEATURE' ? mapml.getElementsByTagName('geometry')[0] : mapml,
-        cs = geometry.getAttribute("cs") || nativeCS, group = [], svgGroup = L.SVG.create('g');
+        cs = geometry.getAttribute("cs") || nativeCS, group = [], svgGroup = L.SVG.create('g'), copyOptions = Object.assign({}, vectorOptions);
     for(let geo of geometry.querySelectorAll('polygon, linestring, multilinestring, point, multipoint')){
-      group.push(M.feature(geo, Object.assign(vectorOptions,
+      group.push(M.feature(geo, Object.assign(copyOptions,
         { nativeCS: cs,
           nativeZoom: zoom,
           projection: this.options.projection,
