6c4a42cb6053eb8870c7ca97d215b64943240430
ben-lu-uw <b54lu@uwaterloo.ca>
2021-09-16 10:56:48 -0400

    Refactor tests tile element to map-tile

--- test/e2e/layers/mapMLStaticTileLayer.test.js
+++ test/e2e/layers/mapMLStaticTileLayer.test.js
@@ -52,7 +52,7 @@ jest.setTimeout(50000);
           test("[" + browserType + "]" + " Tiles load in on default map zoom level", async () => {
             const tiles = await page.$eval(
               "xpath=//html/body/map/div >> css=div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > div > div.leaflet-layer.mapml-static-tile-layer > div",
-              (tileGroup) => tileGroup.getElementsByTagName("tile").length
+              (tileGroup) => tileGroup.getElementsByTagName("map-tile").length
             );
             expect(tiles).toEqual(1);
           });
