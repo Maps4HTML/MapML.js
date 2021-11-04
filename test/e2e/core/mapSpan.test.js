a5a4bf8b15f0fe605fcae4c3c34b5fa71669d979
ben-lu-uw <b54lu@uwaterloo.ca>
2021-11-04 15:27:54 -0400

    Fix white space parsing issue and add test for it

--- test/e2e/core/mapSpan.test.js
+++ test/e2e/core/mapSpan.test.js
@@ -23,16 +23,16 @@ jest.setTimeout(50000);
 
                 test("[" + browserType + "]" + " <map-span> hides tile boundaries", async ()=>{
                     const total = await page.$eval(
-                        'body > mapml-viewer div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > div > div.leaflet-layer.mapml-templatedlayer-container > div > div > div:nth-child(1) > svg > g > g:nth-child(1) > path:nth-child(2)',
+                        'body > mapml-viewer:nth-child(1) div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > div > div.leaflet-layer.mapml-templatedlayer-container > div > div > div:nth-child(1) > svg > g > g:nth-child(1) > path:nth-child(2)',
                         (path) => path.getAttribute("style")
                     );
 
                     const featureOutline = await page.$(
-                        'body > mapml-viewer div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > div > div.leaflet-layer.mapml-templatedlayer-container > div > div > div:nth-child(1) > svg > g > g:nth-child(1) > path.fclass._2.mapml-feature-outline'
+                        'body > mapml-viewer:nth-child(1) div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > div > div.leaflet-layer.mapml-templatedlayer-container > div > div > div:nth-child(1) > svg > g > g:nth-child(1) > path.fclass._2.mapml-feature-outline'
                     );
 
                     const hidden = await page.$eval(
-                        'body > mapml-viewer div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > div > div.leaflet-layer.mapml-templatedlayer-container > div > div > div:nth-child(1) > svg > g > g:nth-child(1) > path.noline.fclass._2',
+                        'body > mapml-viewer:nth-child(1) div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > div > div.leaflet-layer.mapml-templatedlayer-container > div > div > div:nth-child(1) > svg > g > g:nth-child(1) > path.noline.fclass._2',
                         (path) => path.getAttribute("d")
                     );
                     expect(featureOutline).not.toBe(null);
@@ -44,6 +44,17 @@ jest.setTimeout(50000);
 
                     expect(total).toEqual("stroke: none;");
                     expect(index).toEqual(-1);
+                });
+
+                //https://github.com/Maps4HTML/Web-Map-Custom-Element/issues/559#issuecomment-959805896
+                test("[" + browserType + "]" + " White space parsing for map-coordinates", async ()=>{
+                    await page.waitForTimeout(1000);
+                    const feature = await page.$eval(
+                        'body > mapml-viewer:nth-child(2) div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-overlay-pane > div > div.leaflet-layer.mapml-templatedlayer-container > div > div > div:nth-child(1) > svg > g > g:nth-child(8) > path.fclass.mapml-feature-outline',
+                        (path) => path.getAttribute("d")
+                    );
+
+                    expect(feature).toEqual("M0 217L0 217L0 217L2 217L4 218L6 218L6 218L6 216L2 214L0 216L0 216");
                 });
             }
         );
