1111728f186d5229d0bb5d99264fdac060f8bd17
ben-lu-uw <69722289+ben-lu-uw@users.noreply.github.com>
2021-11-10 18:24:50 -0500

    Fix moveend firing when zooming out/in at min/max (#567)
    * Redefine onKeyDown to consider max/min zoom level
    
    * Add test for new onKeyDown function
    
    * Use L.point instead of toPoint

---
+++ src/mapml/keyboard.js
@@ -0,0 +1,50 @@
+L.Map.Keyboard.include({
+    _onKeyDown: function (e) {
+
+        if (e.altKey || e.ctrlKey || e.metaKey) { return; }
+
+        let zoomIn = {
+            187: 187,
+            107: 107,
+            61: 61,
+            171: 171
+        };
+
+        let zoomOut = {
+            189: 189,
+            109: 109,
+            54: 54,
+            173: 173
+        };
+
+        var key = e.keyCode,
+            map = this._map,
+            offset;
+
+        if (key in this._panKeys) {
+            if (!map._panAnim || !map._panAnim._inProgress) {
+                offset = this._panKeys[key];
+                if (e.shiftKey) {
+                    offset = L.point(offset).multiplyBy(3);
+                }
+
+                map.panBy(offset);
+
+                if (map.options.maxBounds) {
+                    map.panInsideBounds(map.options.maxBounds);
+                }
+            }
+        } else if (key in this._zoomKeys) {
+            if((key in zoomIn && map._layersMaxZoom !== map.getZoom()) || (key in zoomOut && map._layersMinZoom !== map.getZoom()))
+                map.setZoom(map.getZoom() + (e.shiftKey ? 3 : 1) * this._zoomKeys[key]);
+
+        } else if (key === 27 && map._popup && map._popup.options.closeOnEscapeKey) {
+            map.closePopup();
+
+        } else {
+            return;
+        }
+
+        stop(e);
+    }
+});
