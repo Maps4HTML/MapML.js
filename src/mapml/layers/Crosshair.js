03576903d4a900c9688638cc0a79eb60eb3eb25c
Malvoz <26493779+Malvoz@users.noreply.github.com>
2021-12-02 21:30:15 +0100

    Crosshair: replace `style` with presentational attributes (and optimize SVG)

--- src/mapml/layers/Crosshair.js
+++ src/mapml/layers/Crosshair.js
@@ -1,32 +1,9 @@
 export var Crosshair = L.Layer.extend({
   onAdd: function (map) {
 
-    //SVG crosshair design from https://github.com/xguaita/Leaflet.MapCenterCoord/blob/master/src/icons/MapCenterCoordIcon1.svg?short_path=81a5c76
-    let svgInnerHTML = `<svg
-    xmlns:dc="http://purl.org/dc/elements/1.1/"
-    xmlns:cc="http://creativecommons.org/ns#"
-    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-    xmlns:svg="http://www.w3.org/2000/svg"
-    xmlns="http://www.w3.org/2000/svg"
-    version="1.1"
-    x="0px"
-    y="0px"
-    viewBox="0 0 99.999998 99.999998"
-    xml:space="preserve">
-    <g><circle
-        r="3.9234731"
-        cy="50.21946"
-        cx="50.027821"
-        style="color:#000000;clip-rule:nonzero;display:inline;overflow:visible;isolation:auto;mix-blend-mode:normal;color-interpolation:sRGB;color-interpolation-filters:linearRGB;solid-color:#000000;solid-opacity:1;fill:#000000;fill-opacity:1;fill-rule:nonzero;stroke:#ffffff;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;color-rendering:auto;image-rendering:auto;shape-rendering:auto;text-rendering:auto;enable-background:accumulate" /><path
-        d="m 4.9734042,54.423642 31.7671398,0 c 2.322349,0 4.204185,-1.881836 4.204185,-4.204185 0,-2.322349 -1.881836,-4.204184 -4.204185,-4.204184 l -31.7671398,0 c -2.3223489,-2.82e-4 -4.20418433,1.881554 -4.20418433,4.204184 0,2.322631 1.88183543,4.204185 4.20418433,4.204185 z"
-        style="color:#000000;clip-rule:nonzero;display:inline;overflow:visible;isolation:auto;mix-blend-mode:normal;color-interpolation:sRGB;color-interpolation-filters:linearRGB;solid-color:#000000;solid-opacity:1;fill:#000000;fill-opacity:1;fill-rule:nonzero;stroke:#ffffff;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;color-rendering:auto;image-rendering:auto;shape-rendering:auto;text-rendering:auto;enable-background:accumulate" /><path
-        d="m 54.232003,5.1650429 c 0,-2.3223489 -1.881836,-4.20418433 -4.204184,-4.20418433 -2.322349,0 -4.204185,1.88183543 -4.204185,4.20418433 l 0,31.7671401 c 0,2.322349 1.881836,4.204184 4.204185,4.204184 2.322348,0 4.204184,-1.881835 4.204184,-4.204184 l 0,-31.7671401 z"
-        style="fill:#000000;stroke:#ffffff;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;fill-opacity:1" /><path
-        d="m 99.287826,50.219457 c 0,-2.322349 -1.881835,-4.204184 -4.204184,-4.204184 l -31.76714,0 c -2.322349,0 -4.204184,1.881835 -4.204184,4.204184 0,2.322349 1.881835,4.204185 4.204184,4.204185 l 31.76714,0 c 2.320658,0 4.204184,-1.881836 4.204184,-4.204185 z"
-        style="color:#000000;clip-rule:nonzero;display:inline;overflow:visible;isolation:auto;mix-blend-mode:normal;color-interpolation:sRGB;color-interpolation-filters:linearRGB;solid-color:#000000;solid-opacity:1;fill:#000000;fill-opacity:1;fill-rule:nonzero;stroke:#ffffff;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;color-rendering:auto;image-rendering:auto;shape-rendering:auto;text-rendering:auto;enable-background:accumulate" /><path
-        d="m 45.823352,95.27359 c 0,2.322349 1.881836,4.204184 4.204185,4.204184 2.322349,0 4.204184,-1.881835 4.204184,-4.204184 l 0,-31.76714 c 0,-2.322349 -1.881835,-4.204185 -4.204184,-4.204185 -2.322349,0 -4.204185,1.881836 -4.204185,4.204185 l 0,31.76714 z"
-        style="color:#000000;clip-rule:nonzero;display:inline;overflow:visible;isolation:auto;mix-blend-mode:normal;color-interpolation:sRGB;color-interpolation-filters:linearRGB;solid-color:#000000;solid-opacity:1;fill:#000000;fill-opacity:1;fill-rule:nonzero;stroke:#ffffff;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;color-rendering:auto;image-rendering:auto;shape-rendering:auto;text-rendering:auto;enable-background:accumulate" /></g></svg>
- `;
+    // SVG crosshair design from https://github.com/xguaita/Leaflet.MapCenterCoord/blob/master/src/icons/MapCenterCoordIcon1.svg?short_path=81a5c76
+    // Optimized with SVGOMG: https://jakearchibald.github.io/svgomg/
+    let svgInnerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 100 100"><g stroke="#fff" stroke-linecap="round" stroke-linejoin="round"><circle cx="50.028" cy="50.219" r="3.923" stroke-width="2" color="currentColor" overflow="visible"/><path stroke-width="3" d="M4.973 54.424h31.768a4.204 4.204 0 1 0 0-8.409H4.973A4.203 4.203 0 0 0 .77 50.22a4.203 4.203 0 0 0 4.204 4.205z" color="currentColor" overflow="visible"/><path stroke-width="3" d="M54.232 5.165a4.204 4.204 0 1 0-8.408 0v31.767a4.204 4.204 0 1 0 8.408 0V5.165z"/><path stroke-width="3" d="M99.288 50.22a4.204 4.204 0 0 0-4.204-4.205H63.317a4.204 4.204 0 1 0 0 8.409h31.767a4.205 4.205 0 0 0 4.204-4.205zM45.823 95.274a4.204 4.204 0 1 0 8.409 0V63.506a4.204 4.204 0 1 0-8.409 0v31.768z" color="currentColor" overflow="visible"/></g></svg>`;
 
     this._container = L.DomUtil.create("div", "mapml-crosshair", map._container);
     this._container.innerHTML = svgInnerHTML;
@@ -57,9 +34,9 @@ export var Crosshair = L.Layer.extend({
 
   _addOrRemoveCrosshair: function (e) {
     if (this._hasQueryableLayer()) {
-      this._container.style.visibility = null;
+      this._container.removeAttribute("hidden");
     } else {
-      this._container.style.visibility = "hidden";
+      this._container.setAttribute("hidden", "");
     }
   },
 
--- test/e2e/core/reticle.test.js
+++ test/e2e/core/reticle.test.js
@@ -9,11 +9,11 @@ describe("Playwright Keyboard Navigation + Query Layer Tests" , () => {
 
   describe("Crosshair Reticle Tests", () => {
     test("Crosshair hidden onload, shows on focus", async () => {
-      const beforeTabHidden = await page.$eval("div > div.mapml-crosshair", (div) => div.style.visibility);
+      const beforeTabHidden = await page.$eval("div > div.mapml-crosshair", (div) => window.getComputedStyle(div).getPropertyValue("display"));
       await page.keyboard.press("Tab");
-      const afterTab = await page.$eval("div > div.mapml-crosshair", (div) => div.style.visibility);
-      await expect(beforeTabHidden).toEqual("hidden");
-      await expect(afterTab).toEqual("");
+      const afterTab = await page.$eval("div > div.mapml-crosshair", (div) => window.getComputedStyle(div).getPropertyValue("display"));
+      await expect(beforeTabHidden).toEqual("none");
+      await expect(afterTab).toEqual("block");
     });
 
     test("Crosshair remains on map move with arrow keys", async () => {
@@ -25,22 +25,22 @@ describe("Playwright Keyboard Navigation + Query Layer Tests" , () => {
       await page.waitForTimeout(1000);
       await page.keyboard.press("ArrowRight");
       await page.waitForTimeout(1000);
-      const afterMove = await page.$eval("div > div.mapml-crosshair", (div) => div.style.visibility);
-      await expect(afterMove).toEqual("");
+      const afterMove = await page.$eval("div > div.mapml-crosshair", (div) => window.getComputedStyle(div).getPropertyValue("display"));
+      await expect(afterMove).toEqual("block");
     });
 
     test("Crosshair shows on esc but hidden on tab out", async () => {
       await page.keyboard.press("Escape");
-      const afterEsc = await page.$eval("div > div.mapml-crosshair", (div) => div.style.visibility);
+      const afterEsc = await page.$eval("div > div.mapml-crosshair", (div) => window.getComputedStyle(div).getPropertyValue("display"));
       await page.click("body");
       await page.keyboard.press("Tab");
       await page.keyboard.press("ArrowUp");
 
       await page.keyboard.press("Tab");
-      const afterTab = await page.$eval("div > div.mapml-crosshair", (div) => div.style.visibility);
+      const afterTab = await page.$eval("div > div.mapml-crosshair", (div) => window.getComputedStyle(div).getPropertyValue("display"));
 
-      await expect(afterEsc).toEqual("");
-      await expect(afterTab).toEqual("hidden");
+      await expect(afterEsc).toEqual("block");
+      await expect(afterTab).toEqual("none");
     });
 
     test("Crosshair hidden when queryable layer is unselected, shows on reselect", async () => {
@@ -48,13 +48,13 @@ describe("Playwright Keyboard Navigation + Query Layer Tests" , () => {
       await page.keyboard.press("Tab");
       await page.keyboard.press("ArrowUp");
       await page.evaluateHandle(() => document.querySelector("layer-").removeAttribute("checked"));
-      const afterUncheck = await page.$eval("div > div.mapml-crosshair", (div) => div.style.visibility);
+      const afterUncheck = await page.$eval("div > div.mapml-crosshair", (div) => window.getComputedStyle(div).getPropertyValue("display"));
 
       await page.evaluateHandle(() => document.querySelector("layer-").setAttribute("checked", ""));
-      const afterCheck = await page.$eval("div > div.mapml-crosshair", (div) => div.style.visibility);
+      const afterCheck = await page.$eval("div > div.mapml-crosshair", (div) => window.getComputedStyle(div).getPropertyValue("display"));
 
-      await expect(afterUncheck).toEqual("hidden");
-      await expect(afterCheck).toEqual("");
+      await expect(afterUncheck).toEqual("none");
+      await expect(afterCheck).toEqual("block");
     });
   });
 });
