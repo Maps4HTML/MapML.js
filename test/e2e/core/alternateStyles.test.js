d14cc57017b9ca5c4d7cbac9007e57251fe4de0d
Peter Rushforth <peter.rushforth@gmail.com>
2021-12-21 10:42:26 -0500

    Add test

--- test/e2e/core/alternateStyles.test.js
+++ test/e2e/core/alternateStyles.test.js
@@ -1,5 +1,5 @@
 describe("Alternate / Named Styles test", () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     await page.goto(PATH + "alternateStyles.html");
   });
 
@@ -27,7 +27,21 @@ describe("Alternate / Named Styles test", () => {
       await page.keyboard.press("ArrowDown");
       await page.keyboard.press("ArrowDown");
       await page.keyboard.press("Tab");
-      let layerSrcShouldBe = await page.evaluate(`new URL("alternate-styles-imagery.mapml",document.querySelector('layer-').src).toString()`);
+      let layerSrcShouldBe = await page.evaluate(`new URL("alternate-styles-imagery.mapml",window.location).toString()`);
+      let layerSrcIs =await page.evaluate(`document.querySelector('layer-').src`);
+      await expect(layerSrcIs).toEqual(layerSrcShouldBe);
+  });
+
+  test("Mouse activate third alternate style", async () => {
+      await page.hover(".leaflet-top.leaflet-right");
+      await page.click(".mapml-layer-item-settings-control.mapml-button");
+      await page.click(".mapml-layer-item-style");
+      await page.click('text="Satellite Imagery"')
+      let startingLayerSrc = await page.evaluate(`document.querySelector('layer-').src`);
+      await expect(startingLayerSrc).toEqual("alternate-styles-natgeo.mapml");
+      // the focusout event is when the update occurs
+      await page.click('body');
+      let layerSrcShouldBe = await page.evaluate(`new URL("alternate-styles-imagery.mapml",window.location).toString()`);
       let layerSrcIs =await page.evaluate(`document.querySelector('layer-').src`);
       await expect(layerSrcIs).toEqual(layerSrcShouldBe);
   });
