b7d91b91f1ed217403ba4abb2d708aa29a42e0c3
yhy0217 <yhy0218th@gmail.com>
2023-02-16 11:59:33 -0500

    Add comment

--- test/e2e/layers/templatedTileLayer.test.js
+++ test/e2e/layers/templatedTileLayer.test.js
@@ -49,14 +49,15 @@ test.describe("Playwright mapMLTemplatedTile Layer Tests", () => {
       expect(tiles).toEqual(8);
     });
 
-    test("templated tile layer work as expectation without <map-input> for zoom level", async () => {
+    test("Templated tile layer works without <map-input> for zoom level", async () => {
+      // tests fix for https://github.com/Maps4HTML/Web-Map-Custom-Element/issues/669
       await page.hover("div > div.leaflet-control-container > div.leaflet-top.leaflet-right > div");
       const layerCount = await page.$eval(
         "div > div.leaflet-control-container > div.leaflet-top.leaflet-right > div > section > div.leaflet-control-layers-overlays",
         (el) => el.children.length
       );
       expect(layerCount).toEqual(3);
-    })
+    });
   });
 
 });
