c876387ad8c298e76189f4dbfdf55dfbc67e30ed
yhy0217 <yhy0218th@gmail.com>
2023-03-01 13:19:30 -0500

    Add empty templates for select, meta and geometry

---
+++ src/map-meta.js
@@ -0,0 +1,51 @@
+export class MapMeta extends HTMLElement {
+  static get observedAttributes() {
+    return ['name','content'];
+  }
+  get name() {
+    return this.getAttribute('name');
+  }
+  set name(val) {
+    if (['projection','extent','cs','zoom'].includes(val)) {
+      this.setAttribute('name', val);
+    }
+  }
+  get content() {
+    return this.getAttribute('content');
+  }
+  set content(val) {
+    // improve this
+    if (this.name === 'cs' && !['tcrs','tilematrix','pcrs','gcrs','map','tile'].includes(val)) {
+      return;
+    }
+    this.setAttribute('content', val);
+  }
+  
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case 'name': {
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      }
+      case 'content': {
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      }
+    }
+  }
+  constructor() {
+    // Always call super first in constructor
+    super();    
+  }
+  connectedCallback() {
+    
+  }
+  disconnectedCallback() {
+    
+  }
+};
+window.customElements.define('map-meta', MapMeta);
---
+++ src/map-select.js
@@ -0,0 +1,43 @@
+export class MapSelect extends HTMLElement {
+  static get observedAttributes() {
+    return ['name','id'];
+  }
+  get name() {
+    return this.getAttribute('name');
+  }
+  set name(val) {
+    this.setAttribute('name', val);
+  }
+  get id() {
+    return this.getAttribute('id');
+  }
+  set id(val) {
+    this.setAttribute('id', val);
+  }
+  
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case 'name':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'id':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+    }
+  }
+  constructor() {
+    // Always call super first in constructor
+    super();    
+  }
+  connectedCallback() {
+    
+  }
+  disconnectedCallback() {
+    
+  }
+};
+window.customElements.define('map-select', MapSelect);
