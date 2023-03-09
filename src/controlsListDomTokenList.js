9b7179d8ace30e0571d607fcc31cd48d47a660f3
prushfor <prushfor@L-BSC-A146390.nrn.nrcan.gc.ca>
2023-03-09 12:12:09 -0500

    Upgrade jshint; ignore private fields for linting as this is not yet supported by jshint

--- src/controlsListDomTokenList.js
+++ src/controlsListDomTokenList.js
@@ -1,6 +1,8 @@
 export default class DOMTokenList {
+    /* jshint ignore:start */
 	#mapEl; // create mapEl as a private property
 	#valueSet;
+    /* jshint ignore:end */
 	constructor (initialValue, mapEl) {
 		// create donor/host div to extract DomTokenList from
 		const hostingElement = document.createElement('div');
@@ -8,13 +10,13 @@ export default class DOMTokenList {
 		
 		// to check if value is being set, protects from infinite recursion
 		// from attributeChangedCallback of mapml-viewer and web-map
-		this.#valueSet = false;
+		this.#valueSet = false;// jshint ignore:line
 		this.controlsList.value = initialValue ?? '';
-		this.#mapEl = mapEl;
+		this.#mapEl = mapEl;// jshint ignore:line
 	}
 	
 	get valueSet () {
-		return this.#valueSet;
+		return this.#valueSet;// jshint ignore:line
 	}
 
 	get length () {
@@ -26,9 +28,11 @@ export default class DOMTokenList {
 	}
 	set value (val) {
 		this.controlsList.value = val.toLowerCase();
+        /* jshint ignore:start */
 		this.#valueSet = true;
 		this.#mapEl.setAttribute("controlslist", this.controlsList.value);
 		this.#valueSet = false;
+        /* jshint ignore:end */
 	}
 
 	item (index) {
@@ -42,19 +46,19 @@ export default class DOMTokenList {
 	// Modified default behavior
 	add (token) {
 		this.controlsList.add(token);
-		this.#mapEl.setAttribute("controlslist", this.controlsList.value);
+		this.#mapEl.setAttribute("controlslist", this.controlsList.value);// jshint ignore:line
 	}
 
 	// Modified default behavior
 	remove (token) {
 		this.controlsList.remove(token);
-		this.#mapEl.setAttribute("controlslist", this.controlsList.value);
+		this.#mapEl.setAttribute("controlslist", this.controlsList.value);// jshint ignore:line
 	}
 
 	// Modified default behavior
 	replace (oldToken, newToken) {
 		this.controlsList.replace(oldToken, newToken);
-		this.#mapEl.setAttribute("controlslist", this.controlsList.value);
+		this.#mapEl.setAttribute("controlslist", this.controlsList.value);// jshint ignore:line
 	}
 
 	// Modified default behavior
@@ -71,7 +75,7 @@ export default class DOMTokenList {
 	//https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList/toggle
 	toggle (token, force) {
 		this.controlsList.toggle(token, force);
-		this.#mapEl.setAttribute("controlslist", this.controlsList.value);
+		this.#mapEl.setAttribute("controlslist", this.controlsList.value);// jshint ignore:line
 	}
 
 	entries () {
