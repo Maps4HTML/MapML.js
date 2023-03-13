c3cc162af50abbd4e7a2035c4cd1e75ccc8c8a62
Peter Rushforth <peter.rushforth@gmail.com>
2023-03-13 17:34:07 -0400

    Rename the layer-.focus() method to layer-.zoomTo()

--- src/mapml/handlers/ContextMenu.js
+++ src/mapml/handlers/ContextMenu.js
@@ -202,7 +202,7 @@ export var ContextMenu = L.Handler.extend({
 
   _zoomToLayer: function (e) {
     let context = e instanceof KeyboardEvent ? this._map.contextMenu : this.contextMenu;
-    context._layerClicked.layer._layerEl.focus();
+    context._layerClicked.layer._layerEl.zoomTo();
   },
 
   _copyLayer: function (e) {
