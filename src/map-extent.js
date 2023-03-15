d580797927f8b6f520a7b05648e3f6fee42a2961
Peter Rushforth <peter.rushforth@gmail.com>
2023-03-15 15:36:23 -0400

    Add ce boilerplate (#799)
    * Add extent,input and link template custom elements
    
    * Add empty templates for select, meta and geometry
    
    * map-a and map-feature templates
    
    * add properties,span,style
    
    * Fix lint / JSHint messages
    
    ---------
    
    Co-authored-by: prushfor <prushfor@L-BSC-A146390.nrn.nrcan.gc.ca>
    Co-authored-by: yhy0217 <yhy0218th@gmail.com>
    Co-authored-by: Jacky0299 <jackyissuperman@gmail.com>
    Co-authored-by: Kevin Kim <kimkevi912@gmail.com>

---
+++ src/map-extent.js
@@ -0,0 +1,80 @@
+import { MapInput } from './map-input.js';
+import { MapLink } from './map-link.js';
+
+export class MapExtent extends HTMLElement {
+  static get observedAttributes() {
+    return ['units','checked','label','opacity'];
+  }
+  get units() {
+   return this.getAttribute('units');
+  }
+  set units(val) {
+    // built in support for OSMTILE, CBMTILE, WGS84 and APSTILE
+    if (['OSMTILE','CBMTILE','WGS84','APSTILE'].includes(val)) {
+      this.setAttribute('units',val);
+    }
+    // else need to check with the mapml-viewer element if the custom projection is defined
+  }
+  get checked() {
+    return this.hasAttribute('checked');
+  }
+  
+  set checked(val) {
+    if (val) {
+      this.setAttribute('checked', '');
+    } else {
+      this.removeAttribute('checked');
+    }
+  }
+  get label() {
+    return this.hasAttribute('label')?this.getAttribute('label'):'';
+  }
+  set label(val) {
+    if (val) {
+      this.setAttribute('label',val);
+    }
+  }
+  get opacity(){
+    return this._opacity;
+  }
+
+  set opacity(val) {
+    if(+val > 1 || +val < 0) return;
+    this.setAttribute('opacity', val);
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch(name) {
+      case 'units':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'label': 
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'checked': 
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'opacity':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+    }
+  }
+  constructor() {
+    // Always call super first in constructor
+    super();    
+  }
+  connectedCallback() {
+    
+  }
+  disconnectedCallback() {
+    
+  }
+}
+window.customElements.define('map-extent', MapExtent);
---
+++ src/map-input.js
@@ -0,0 +1,153 @@
+import { MapLink } from './map-link.js';
+
+export class MapInput extends HTMLElement {
+  static get observedAttributes() {
+    return ['name','type','value','axis','units','position','rel','min','max','step'];
+  }
+
+  get name() {
+    return this.getAttribute('name');
+  }
+  set name(val) {
+    if (val) {
+      this.setAttribute('name', val);
+    }
+  }
+  get type() {
+    return this.getAttribute('type');
+  }
+  set type(val) {
+    if (['location'].includes(val)) {
+      this.setAttribute('type', val);
+    }
+  }
+  get value() {
+    return this.getAttribute('value');
+  }
+  set value(val) {
+    if (val) {
+      this.setAttribute('value', val);
+    }
+  }
+  get axis() {
+    return this.getAttribute('axis');
+  }
+  set axis(val) {
+    if (val) {
+      this.setAttribute('axis', val);
+    }
+  }
+  get units() {
+    return this.getAttribute('units');
+  }
+  set units(val) {
+    if (val) {
+      this.setAttribute('units', val);
+    }
+  }
+  get position() {
+    return this.getAttribute('position');
+  }
+  set position(val) {
+    if (val) {
+      this.setAttribute('position', val);
+    }
+  }
+  get rel() {
+    return this.getAttribute('rel');
+  }
+  set rel(val) {
+    if (val) {
+      this.setAttribute('rel', val);
+    }
+  }
+  get min() {
+    return this.getAttribute('min');
+  }
+  set min(val) {
+    if (val) {
+      this.setAttribute('min', val);
+    }
+  }
+  get max() {
+    return this.getAttribute('max');
+  }
+  set max(val) {
+    if (val) {
+      this.setAttribute('max', val);
+    }
+  }
+  get step() {
+    return this.getAttribute('step');
+  }
+  set step(val) {
+    if (val) {
+      this.setAttribute('step', val);
+    }
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case 'name':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'type':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'value':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'axis':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'units':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'position':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'rel':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'min':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'max':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+      case 'step':
+        if (oldValue !== newValue) {
+          // handle side effects
+        }
+        break;
+    }
+  }
+  constructor() {
+    // Always call super first in constructor
+    super();    
+  }
+  connectedCallback() {
+    
+  }
+  disconnectedCallback() {
+    
+  }
+}
+window.customElements.define('map-input', MapInput);
