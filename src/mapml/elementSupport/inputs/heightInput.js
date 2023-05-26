1c168bc42af2c7cf535f7400326c5f36f377571a
AliyanH <aliyanulhaq@gmail.com>
2023-05-26 14:52:21 -0400

    implement Classes for different types of map-input types

--- src/mapml/elementSupport/inputs/heightInput.js
+++ src/mapml/elementSupport/inputs/heightInput.js
@@ -1,12 +1,18 @@
 export class HeightInput {
-	constructor(name) {
-	  this.name = name;
-	}
-  
-	validateInput() {
-	  // name is required
-	  // 
-	  // checks here...
-	  return;
-	}
+  constructor(name, layer) {
+    this.name = name;
+    this.layer = layer;
+  }
+
+  validateInput() {
+    // name is required
+    if (!this.name) {
+      return false;
+    }
+    return true;
+  }
+
+  getValue() {
+    return this.layer._map.getSize().y;
+  }
 }
--- src/mapml/elementSupport/inputs/locationInput.js
+++ src/mapml/elementSupport/inputs/locationInput.js
@@ -1,22 +1,86 @@
 export class LocationInput {
-	constructor(name, position, axis, cs, min, max, rel) {
-	  this.name = name;
-	  this.position = position;
-	  this.axis = axis;
-	  this.cs = cs; // units
-	  this.min = min;
-	  this.max = max;
-	  this.rel = rel;
-	}
-  
-	validateInput() {
-	  // name is required
-	  // axis is required
-	  // cs is required
-	  // position is not required, will default to top-left
-	  // min max fallbacks, map-meta -> projection
-	  // rel not required, default is image/extent
-	  // checks here...
-	  return;
-	}
+  constructor(name, position, axis, units, min, max, rel, layer) {
+    this.name = name;
+    this.position = position;
+    this.axis = axis;
+    this.units = units; // cs
+    this.min = min;
+    this.max = max;
+    this.rel = rel;
+    this.layer = layer;
+  }
+
+  validateInput() {
+    // name is required
+    // axis is required
+    // cs/units is required
+    if (!this.name || !this.axis || !this.units) {
+      return false;
+    }
+    // check if axis match the cs
+    let axisCS = M.axisToCS(this.axis);
+    if (
+      (typeof axisCS === 'string' && axisCS !== this.units) ||
+      (typeof axisCS === 'object' &&
+        (axisCS[0] !== this.units || axisCS[1] !== this.units))
+    ) {
+      return false;
+    }
+    // position is not required, will default to top-left
+    // min max fallbacks, map-meta -> projection
+    // rel not required, default is image/extent
+    return true;
+  }
+
+  _TCRSToPCRS(coords, zoom) {
+    // TCRS pixel point to Projected CRS point (in meters, presumably)
+    var map = this.layer._map,
+      crs = map.options.crs,
+      loc = crs.transformation.untransform(coords, crs.scale(zoom));
+    return loc;
+  }
+
+  getValue(zoom = undefined, bounds = undefined) {
+    // units = cs
+    //<input name="..." units="pcrs" type="location" position="top|bottom-left|right" axis="northing|easting|latitude|longitude">
+    if (zoom === undefined) zoom = this.layer._map.getZoom();
+    if (bounds === undefined) bounds = this.layer._map.getPixelBounds();
+
+    if (this.units === 'pcrs' || this.units === 'gcrs') {
+      switch (this.axis) {
+        case 'longitude':
+        case 'easting':
+          if (this.position) {
+            if (this.position.match(/.*?-left/i)) {
+              return this._TCRSToPCRS(bounds.min, zoom).x;
+            } else if (this.position.match(/.*?-right/i)) {
+              return this._TCRSToPCRS(bounds.max, zoom).x;
+            }
+          } else {
+            // position is not required, will default to top-left
+          }
+          break;
+        case 'latitude':
+        case 'northing':
+          if (this.position) {
+            if (this.position.match(/top-.*?/i)) {
+              return this._TCRSToPCRS(bounds.min, zoom).y;
+            } else if (this.position.match(/bottom-.*?/i)) {
+              return this._TCRSToPCRS(bounds.max, zoom).y;
+            }
+          } else {
+            // position is not required, will default to top-left
+          }
+          break;
+      }
+    } else if (
+      this.units === 'tilematrix' ||
+      this.units === 'tcrs' ||
+      this.units === 'tile' ||
+      this.units === 'map'
+    ) {
+      // TODO: What happens here...
+    }
+    return;
+  }
 }
