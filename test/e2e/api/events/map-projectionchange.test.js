df64f96ddcc9f75491b82c243dc616ef31eb848b
Peter Rushforth <peter.rushforth@gmail.com>
2023-11-13 19:34:44 -0500

    Add test for empty map history after map-projectionchange event

--- test/e2e/api/events/map-projectionchange.test.js
+++ test/e2e/api/events/map-projectionchange.test.js
@@ -8,6 +8,10 @@ test.describe('map-projectionchange test ', () => {
     page =
       context.pages().find((page) => page.url() === 'about:blank') ||
       (await context.newPage());
+
+  });
+  
+  test.beforeEach(async function() {
     await page.goto('events/map-projectionchange.html');
   });
 
@@ -15,24 +19,41 @@ test.describe('map-projectionchange test ', () => {
     await context.close();
   });
 
-  test('do something and test it', async () => {
+  test('Multiple map-extents in different projections adapt to map-projectionchange', async () => {
     const viewer = await page.locator('mapml-viewer');
-    expect(await viewer.evaluate((v)=>v.projection)).toEqual('OSMTILE');
-    expect(await viewer.evaluate((v)=>{
-      return v.querySelector('map-extent[units=OSMTILE]').disabled;
-    })).toBe(false);
-    expect(await viewer.evaluate((v)=>{
-      return v.querySelector('map-extent[units=CBMTILE]').disabled;
-    })).toBe(true);
-    await viewer.evaluate(()=> changeProjection());
+    expect(await viewer.evaluate((v) => v.projection)).toEqual('OSMTILE');
+    expect(
+      await viewer.evaluate((v) => {
+        return v.querySelector('map-extent[units=OSMTILE]').disabled;
+      })
+    ).toBe(false);
+    expect(
+      await viewer.evaluate((v) => {
+        return v.querySelector('map-extent[units=CBMTILE]').disabled;
+      })
+    ).toBe(true);
+    await viewer.evaluate(() => changeProjection());
     await page.waitForTimeout(500);
-    expect(await viewer.evaluate((v)=> v.projection)).toEqual('CBMTILE');
-    expect(await viewer.evaluate((v)=>{
-      return v.querySelector('map-extent[units=OSMTILE]').disabled;
-    })).toBe(true);
-    expect(await viewer.evaluate((v)=>{
-      return v.querySelector('map-extent[units=CBMTILE]').disabled;
-    })).toBe(false);
-    
+    expect(await viewer.evaluate((v) => v.projection)).toEqual('CBMTILE');
+    expect(
+      await viewer.evaluate((v) => {
+        return v.querySelector('map-extent[units=OSMTILE]').disabled;
+      })
+    ).toBe(true);
+    expect(
+      await viewer.evaluate((v) => {
+        return v.querySelector('map-extent[units=CBMTILE]').disabled;
+      })
+    ).toBe(false);
+  });
+  test('History is empty after map-projectionchange', async () => {
+    const viewer = await page.locator('mapml-viewer');
+    expect(await viewer.evaluate((v) => v.projection)).toEqual('OSMTILE');
+    await viewer.evaluate(() => changeProjection());
+    await page.waitForTimeout(1000);
+    expect(await viewer.evaluate((v) => v.projection)).toEqual('CBMTILE');
+    const reload = await page.getByLabel('Reload');
+    expect(await reload.evaluate((button)=>button.ariaDisabled)).toBe('true');
+
   });
 });
