809ab3af806f2770622ee9abcc5a8db7dbb964eb
Peter Rushforth <peter.rushforth@gmail.com>
2023-11-13 19:46:34 -0500

    Add test for opacity on layer and map-extent after map-projectionchange

--- test/e2e/api/events/map-projectionchange.test.js
+++ test/e2e/api/events/map-projectionchange.test.js
@@ -8,10 +8,9 @@ test.describe('map-projectionchange test ', () => {
     page =
       context.pages().find((page) => page.url() === 'about:blank') ||
       (await context.newPage());
-
   });
-  
-  test.beforeEach(async function() {
+
+  test.beforeEach(async function () {
     await page.goto('events/map-projectionchange.html');
   });
 
@@ -50,10 +49,22 @@ test.describe('map-projectionchange test ', () => {
     const viewer = await page.locator('mapml-viewer');
     expect(await viewer.evaluate((v) => v.projection)).toEqual('OSMTILE');
     await viewer.evaluate(() => changeProjection());
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(500);
     expect(await viewer.evaluate((v) => v.projection)).toEqual('CBMTILE');
     const reload = await page.getByLabel('Reload');
-    expect(await reload.evaluate((button)=>button.ariaDisabled)).toBe('true');
-
+    expect(await reload.evaluate((button) => button.ariaDisabled)).toBe('true');
+  });
+  test('Opacity is maintained on layer- and map-extent after map-projectionchange', async () => {
+    const layer = await page.locator('layer-');
+    await layer.evaluate((l) => (l.opacity = 0.5));
+    const osmtileExtent = await page.locator('map-extent[units=OSMTILE]');
+    await osmtileExtent.evaluate((e) => (e.opacity = 0.4));
+    const cbmtileExtent = await page.locator('map-extent[units=CBMTILE]');
+    await cbmtileExtent.evaluate((e) => (e.opacity = 0.3));
+    await page.evaluate(() => changeProjection());
+    await page.waitForTimeout(500);
+    expect(await osmtileExtent.evaluate((e) => e.opacity)).toBe(0.4);
+    expect(await cbmtileExtent.evaluate((e) => e.opacity)).toBe(0.3);
+    expect(await layer.evaluate((l) => l.opacity)).toBe(0.5);
   });
 });
