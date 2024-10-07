4be0be4d02772dbbd10203f88cb3ba020b06d7bb
yushan-mu <Yueyue0272@gmail.com>
2024-10-07 15:03:54 -0400

    Added 500ms timeout

--- test/e2e/layers/layerLabel.test.js
+++ test/e2e/layers/layerLabel.test.js
@@ -1,36 +1,33 @@
 import { test, expect, chromium } from '@playwright/test';
 
 test.describe('Layer Label Tests', () => {
- 
-	let page;
-	let context;
-	test.beforeAll(async function () {
-		context = await chromium.launchPersistentContext('');
-		page =
-			context.pages().find((page) => page.url() === 'about:blank') ||
-			(await context.newPage());
-		await page.goto('layerLabel.html');
-	});
+  let page;
+  let context;
+  test.beforeAll(async function () {
+    context = await chromium.launchPersistentContext('');
+    page =
+      context.pages().find((page) => page.url() === 'about:blank') ||
+      (await context.newPage());
+    await page.goto('layerLabel.html');
+  });
 
-	test.afterAll(async function () {
-		await context.close();
-	});
+  test.afterAll(async function () {
+    await context.close();
+  });
 
-	test('Name of unnamed layer is Layer', async () => {
-		const label = await page.locator(
-			'body > mapml-viewer > layer-'
-		);
+  test('Name of unnamed layer is Layer', async () => {
+    const label = await page.locator('body > mapml-viewer > layer-');
 
-		await page.waitForTimeout(50);
-		expect(await label.evaluate(elem => elem.label)).toEqual('Layer');
-	});
+    await page.waitForTimeout(500);
+    expect(await label.evaluate((elem) => elem.label)).toEqual('Layer');
+  });
 
-	test('Unnamed layer shows up as Layer in layer control', async () => {
-		const text = await page.locator(
-			'body > mapml-viewer >> css=div > label.mapml-layer-item-toggle'
-		);
-		expect(await text.evaluate(text => text.textContent)).toEqual('Layer');
-	});
+  test('Unnamed layer shows up as Layer in layer control', async () => {
+    const text = await page.locator(
+      'body > mapml-viewer >> css=div > label.mapml-layer-item-toggle'
+    );
+    expect(await text.evaluate((text) => text.textContent)).toEqual('Layer');
+  });
 });
 
 // to do: check that the thing's name is 'Layer'
